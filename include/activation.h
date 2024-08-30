4bf679c2208212df681ea4726dcc3402e18aae79
Armaggheddon <alebrum97@gmail.com>
2024-08-30 20:22:10 +0200

    Added activation function name

--- include/activation.h
+++ include/activation.h
@@ -2,28 +2,32 @@
 #define ACTIVATION_H
 
 #include <vector>
-
+#include <string>
 
 class ActivationFn{
     public:
+        std::string name;
         virtual void forward(std::vector<float> *matrix) = 0;
         virtual void backward(std::vector<float> *matrix) = 0;
 };
 
 class ReLU : public ActivationFn{
     public:
+        ReLU();
         void forward(std::vector<float> *matrix);
         void backward(std::vector<float> *matrix);
 };
 
 class Sigmoid : public ActivationFn{
     public:
+        Sigmoid();
         void forward(std::vector<float> *matrix);
         void backward(std::vector<float> *matrix);
 };
 
 class Softmax : public ActivationFn{
     public:
+        Softmax();
         void forward(std::vector<float> *matrix);
         void backward(std::vector<float> *matrix);
 };
--- src/activation.cpp
+++ src/activation.cpp
@@ -1,7 +1,12 @@
 #include <cmath>
 #include <vector>
 #include "activation.h"
+#include <string>
 
+
+ReLU::ReLU(){
+    this->name = "relu";
+}
 
 void ReLU::forward(std::vector<float> *matrix){
     for(int i=0; i<matrix->size(); i++) matrix->at(i) = std::max(matrix->at(i), 0.0f);
@@ -11,6 +16,9 @@ void ReLU::backward(std::vector<float> *matrix){
     for(int i=0; i<matrix->size(); i++) matrix->at(i) = matrix->at(i) > 0 ? 1 : 0;
 }
 
+Sigmoid::Sigmoid(){
+    this->name = "sigmoid";
+}
 void Sigmoid::forward(std::vector<float> *matrix){
     for(int i=0; i<matrix->size(); i++) matrix->at(i) = 1.0 / (1.0 + std::exp(-matrix->at(i)));
 }
@@ -19,6 +27,9 @@ void Sigmoid::backward(std::vector<float> *matrix){
     for(int i=0; i<matrix->size(); i++) matrix->at(i) = matrix->at(i) * (1 - matrix->at(i));
 }
 
+Softmax::Softmax(){
+    this->name = "softmax";
+}
 void Softmax::forward(std::vector<float> *matrix){
     float sum = 0;
     for(int i=0; i<matrix->size(); i++) sum += std::exp(matrix->at(i));
