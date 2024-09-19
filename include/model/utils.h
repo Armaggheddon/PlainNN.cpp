0077bfffd0b8e913a0bdfe23fd8f840365276e7f
Armaggheddon <alebrum97@gmail.com>
2024-09-20 00:30:53 +0200

    Refactor utils.h and utils.cpp to add string_to_lower and softmax functions

--- include/model/utils.h
+++ include/model/utils.h
@@ -1,9 +1,24 @@
 #ifndef MODEL_UTILS_H
 #define MODEL_UTILS_H
 
+#include "tensor.h"
 #include <string>
 #include <algorithm>
 
+/**
+ * @brief Convert a string to lower case
+ * 
+ * @param str The string to convert
+ * @return std::string The converted string
+ */
 std::string string_to_lower(std::string str);
+
+/**
+ * @brief Compute the softmax of a tensor
+ * 
+ * @param input The input tensor
+ * @return Tensor The softmax of the input tensor
+ */
+Tensor softmax(Tensor& input);
 
 #endif // MODEL_UTILS_H
