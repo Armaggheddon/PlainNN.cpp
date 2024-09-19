02e61bb3b89fc228be22b25f6d8205c659289815
Armaggheddon <alebrum97@gmail.com>
2024-09-20 00:30:22 +0200

    Added tanh activation function

--- include/model/layers/activation_fncs.h
+++ include/model/layers/activation_fncs.h
@@ -1,5 +1,5 @@
-#ifndef MODEL_LAYERS_ACTIVATION_FNC_H
-#define MODEL_LAYERS_ACTIVATION_FNC_H
+#ifndef MODEL_LAYERS_ACTIVATION_FNCS_H
+#define MODEL_LAYERS_ACTIVATION_FNCS_H
 
 #include <cmath>
 #include <string>
@@ -8,32 +8,73 @@ enum ActivationType{
     NONE,
     RELU,
     SIGMOID,
-    SOFTMAX
+    TANH
 };
 
 const std::string ACTIVATION_NAMES[] = {
     "None",
     "ReLU",
     "Sigmoid",
-    "Softmax"
+    "Tanh"
 };
 
-
+/**
+ * @brief Abstract class for activation functions, new
+ * activation functions should inherit from this class
+ * and implement all of its methods
+ */
 class ActivationFn{
     public:
         ~ActivationFn(){};
 
         ActivationType fn_type;
 
+        /**
+         * @brief Forward pass of the activation function
+         * 
+         * @param input The input to the activation function
+         * @return double The output of the activation function
+         */
         virtual double forward(const double input) = 0;
+
+        /**
+         * @brief Backward pass of the activation function
+         * 
+         * @param input The input to the activation function
+         * @return double The gradient of the activation function
+         */
         virtual double backward(const double input) = 0;
 
+        /**
+         * @brief Get the name of the activation function
+         * 
+         * @return std::string The name of the activation function
+         */
         std::string name(){return ACTIVATION_NAMES[fn_type];}
+
+        /**
+         * @brief Get the type of the activation function
+         * 
+         * @return ActivationType The type of the activation function
+         */
         ActivationType type(){return fn_type;}
 };
+/**
+ * @brief Get the activation function object from the name
+ * 
+ * @param name The name of the activation function
+ * @return ActivationFn* The activation function object
+ */
 ActivationFn* get_activation_fn_from_name(std::string name);
 
 
+/**
+ * @brief Rectified Linear Unit (ReLU) activation function
+ * 
+ * f(x) = max(0, x)
+ * 
+ * f'(x) = 1 if x > 0, 0 otherwise
+ */
 class ReLU : public ActivationFn{
     public:
         ReLU();
@@ -43,10 +84,34 @@ class ReLU : public ActivationFn{
         double backward(const double input);
 };
 
-
+/**
+ * @brief Sigmoid activation function
+ * 
+ * f(x) = 1 / (1 + exp(-x))
+ * 
+ * f'(x) = f(x) * (1 - f(x))
+ */
 class Sigmoid : public ActivationFn{
     public:
+        
         Sigmoid();
+
+        double forward(const double input);
+
+        double backward(const double input);
+};
+
+/**
+ * @brief Hyperbolic Tangent (Tanh) activation function
+ * 
+ * f(x) = tanh(x)
+ * 
+ * f'(x) = 1 - f(x)^2
+ */
+class Tanh : public ActivationFn{
+    public:
+        
+        Tanh();
 
         double forward(const double input);
 
@@ -55,6 +120,13 @@ class Sigmoid : public ActivationFn{
 
 class None : public ActivationFn{
     public:
+        /**
+         * @brief No activation function
+         * 
+         * f(x) = x
+         * 
+         * f'(x) = 1
+         */
         None();
 
         double forward(const double input);
@@ -62,4 +134,4 @@ class None : public ActivationFn{
         double backward(const double input);
 };
 
-#endif // MODEL_LAYERS_ACTIVATION_FNC_H
+#endif // MODEL_LAYERS_ACTIVATION_FNCS_H
---
+++ src/model/layers/activation_fncs/tanh.cpp
@@ -0,0 +1,15 @@
+#include "activation_fncs.h"
+
+#include <cmath>
+
+Tanh::Tanh(){
+    this->fn_type = ActivationType::TANH;
+}
+
+double Tanh::forward(const double input){
+    return std::tanh(input);
+}
+
+double Tanh::backward(const double input){
+    return 1 - pow(tanh(input), 2);
+}
