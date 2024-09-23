766e0023f24c267f1d7994e4617fbf64e3517794
Armaggheddon <alebrum97@gmail.com>
2024-09-23 19:26:02 +0200

    Added examples from the mnist dataset to be used as inputs

--- live_demo/live_demo_web_ui.py
+++ live_demo/live_demo_web_ui.py
@@ -16,9 +16,14 @@ process = subprocess.Popen(
 )
 
 
-def run_inference(image_dict):
-    pil_image = image_dict["composite"]
+def run_inference(sketchpad_input):
+    
+    pil_image = sketchpad_input["composite"]
+    
     input_vector = np.array(pil_image.resize((28, 28)), dtype=np.float64)
+    # Check if is all zeros
+    if np.all(pil_image == 0):
+        return None
 
     input_vector = input_vector.astype(np.float64) 
     input_vector = input_vector / 255.0  # Normalize pixel values to 0-1
@@ -42,19 +47,41 @@ def run_inference(image_dict):
 
 with gr.Blocks() as interface:
     gr.Markdown("# MNIST Digit Recognition")
+    # Create the components and set render to false
+    # so that items can be rendered in a custom layout
+    result_output = gr.Label(label="Classification Result", show_label=True, num_top_classes=10, render=False)
+    brush = gr.Brush(default_size=20, colors=["white"], color_mode="fixed")
+    sketchpad = gr.Sketchpad(height=500, canvas_size=(400, 400), type='pil', image_mode='L', brush=brush, layers=False, transforms=[], render=False)
     with gr.Row():
         with gr.Column(min_width=500, scale=0):
-            brush = gr.Brush(default_size=30, colors=["white"], color_mode="fixed")
-            sketchpad = gr.Sketchpad(crop_size="1:1", type='pil', image_mode='L', brush=brush)
-            # sketchpad.brush_width = 20
-            btn = gr.Button("Classify Drawing")
+            sketchpad.render()    
+            run_btn = gr.Button(value="Classify Drawing")
+
         with gr.Column():
-            result_output = gr.Label(label="Classification Result", show_label=True, num_top_classes=10)
-    btn.click(fn=run_inference, inputs=sketchpad, outputs=result_output)
+            result_output.render()
+    
+    with gr.Row():
+        gr.Examples(
+            examples=[
+                [f"{this_file_path}/example_images/0.png"],
+                [f"{this_file_path}/example_images/1.png"],
+                [f"{this_file_path}/example_images/2.png"],
+                [f"{this_file_path}/example_images/3.png"],
+                [f"{this_file_path}/example_images/4.png"],
+                [f"{this_file_path}/example_images/5.png"],
+                [f"{this_file_path}/example_images/6.png"],
+                [f"{this_file_path}/example_images/7.png"],
+                [f"{this_file_path}/example_images/8.png"],
+                [f"{this_file_path}/example_images/9.png"],
+            ],
+            inputs=sketchpad
+        )
+
+    run_btn.click(run_inference, inputs=[sketchpad], outputs=[result_output], show_progress=False)
 
 # This is required to use gradio through
 # google Colab
-interface.launch(share=True)
+interface.launch(share=False)
 
 
 
